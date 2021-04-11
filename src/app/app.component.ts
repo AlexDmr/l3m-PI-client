2291bf53e2801bc0503351617f00cd20ce59f0bc
demeurea <alexandre.demeure@univ-grenoble-alpes.fr>
2021-04-12 00:17:15 +0200

    login logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class AppComponent {
   dataIconGoogle = 'assets/images/iconGoogle.png';
+
+  constructor(public auth: AngularFireAuth) {
+  }
+
+  login(): void {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    provider.setCustomParameters({
+      prompt: 'select_account'
+    });
+    this.auth.signInWithPopup(provider);
+  }
+
+  logout(): void {
+    this.auth.signOut();
+  }
 
 }
